b646b224cc390b10033362e470e639e413cc9ed2
Aditya joshi <168006690+Aditya44234@users.noreply.github.com>
2025-11-19 23:07:29 +0530

    Added Frontend url in CORS

--- realestate_api/settings.py
+++ realestate_api/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://market-lens-ai.vercel.app"
     # Add your frontend production URL here when deployed
 ]
 
